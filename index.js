093df77faf26bfe846e5029ee6a97781b672ec26
ilhanzidanie <103909091+ilhanzidanie@users.noreply.github.com>
2023-04-06 00:25:07 +0700

    bug fixed

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 //MENGHITUNG LUAS SEGITIGA
 function hitungluasSegitiga() {
   event.preventDefault();
-  let alas = document.getElementById("alassegitiga").value;
-  let tinggi = document.getElementById("tinggisegitiga").value;
+  let alas = document.getElementById("alasSegitiga").value;
+  let tinggi = document.getElementById("tinggiSegitiga").value;
 
   let luas = 0.5 * alas * tinggi;
 
-  document.getElementById("rumusluassegitiga").innerHTML =
+  document.getElementById("rumusluasSegitiga").innerHTML =
     "L = 1/2 x a x t <br/>" + "L = 1/2 x " + alas + " x " + tinggi;
-  document.getElementById("hasilluassegitiga").innerHTML = "L = " + luas;
+  document.getElementById("hasilluasSegitiga").innerHTML = "L = " + luas;
 }
 
 //MENGHITUNG KELILING SEGITIGA
@@ -19,42 +19,42 @@ function hitungkelilingSegitiga() {
   let sisi3 = parseInt(document.getElementById("sisi3").value);
 
   let keliling = sisi1 + sisi2 + sisi3;
-  document.getElementById("rumuskelilingsegitiga").innerHTML =
+  document.getElementById("rumuskelilingSegitiga").innerHTML =
     "K = S1 + S2 + S3 <br/>" + "K = " + sisi1 + " + " + sisi2 + " + " + sisi3;
-  document.getElementById("hasilkelilingsegitiga").innerHTML =
+  document.getElementById("hasilkelilingSegitiga").innerHTML =
     "K = " + keliling;
 }
 
 //MENGHITUNG LUAS JAJAR GENJANG
 function hitungluasJajar() {
   event.preventDefault();
-  let alas = document.getElementById("alasjajar").value;
-  let tinggi = document.getElementById("tinggijajar").value;
+  let alas = document.getElementById("alasJajar").value;
+  let tinggi = document.getElementById("tinggiJajar").value;
 
   let luas = alas * tinggi;
-  document.getElementById("rumusluasjajar").innerHTML =
+  document.getElementById("rumusluasJajar").innerHTML =
     "L = a x t <br/>" + "L = " + alas + "x" + tinggi;
-  document.getElementById("hasilluasjajar").innerHTML = "L = " + luas;
+  document.getElementById("hasilluasJajar").innerHTML = "L = " + luas;
 }
 
 //MENGHITUNG KELILING JAJAR GENJANG
 function hitungkelilingJajar() {
   event.preventDefault();
-  let panjang = parseInt(document.getElementById("panjangjajar").value);
-  let lebar = parseInt(document.getElementById("lebarjajar").value);
+  let panjang = parseInt(document.getElementById("panjangJajar").value);
+  let lebar = parseInt(document.getElementById("lebarJajar").value);
 
   let keliling = 2 * (panjang + lebar);
-  document.getElementById("rumuskelilingjajar").innerHTML =
+  document.getElementById("rumuskelilingJajar").innerHTML =
     "K = 2 x (a + b) <br/>" + "K = 2 x (" + panjang + " + " + lebar + ")";
-  document.getElementById("hasilkelilingjajar").innerHTML = "K = " + keliling;
+  document.getElementById("hasilkelilingJajar").innerHTML = "K = " + keliling;
 }
 
 //RESET LUAS SEGITIGA
 function resetluasSegitiga() {
-  document.getElementById("alassegitiga").value = "";
-  document.getElementById("tinggisegitiga").value = "";
-  document.getElementById("rumusluassegitiga").innerHTML = "";
-  document.getElementById("hasilluassegitiga").innerHTML = "";
+  document.getElementById("alasSegitiga").value = "";
+  document.getElementById("tinggiSegitiga").value = "";
+  document.getElementById("rumusluasSegitiga").innerHTML = "";
+  document.getElementById("hasilluasSegitiga").innerHTML = "";
 }
 
 //RESET KELILING SEGITIGA
@@ -62,22 +62,22 @@ function resetkelilingSegitiga() {
   document.getElementById("sisi1").value = "";
   document.getElementById("sisi2").value = "";
   document.getElementById("sisi3").value = "";
-  document.getElementById("rumuskelilingsegitiga").innerHTML = "";
-  document.getElementById("hasilkelilingsegitiga").innerHTML = "";
+  document.getElementById("rumuskelilingSegitiga").innerHTML = "";
+  document.getElementById("hasilkelilingSegitiga").innerHTML = "";
 }
 
 //RESET LUAS JAJAR GENJANG
 function resetluasJajar() {
-  document.getElementsById("alasjajar").value = "";
-  document.getElementsById("tinggijajar").value = "";
-  document.getElementsById("rumusluasjajar").innerHTML = "";
-  document.getElementsById("hasilluasjajar").innerHTML = "";
+  document.getElementsById("alasJajar").value = "";
+  document.getElementsById("tinggiJajar").value = "";
+  document.getElementsById("rumusluasJajar").innerHTML = "";
+  document.getElementsById("hasilluasJajar").innerHTML = "";
 }
 
 //RESET KELILING JAJAR GENJANG
 function resetkelilingJajar() {
-  document.getElementById("panjangjajar").value = "";
-  document.getElementById("lebarjajar").value = "";
-  document.getElementById("rumuskelilingjajar").innerHTML = "";
-  document.getElementById("hasilkelilingjajar").innerHTML = "";
+  document.getElementById("panjangJajar").value = "";
+  document.getElementById("lebarJajar").value = "";
+  document.getElementById("rumuskelilingJajar").innerHTML = "";
+  document.getElementById("hasilkelilingJajar").innerHTML = "";
 }
